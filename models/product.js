90de5f565470bd34cb0bd41e28ff7673172b1b3f
Daya Sagar Nune <daya.webtech@gmail.com>
2018-01-14 15:57:55 -0800

    fix indentations - use 2 spaces

--- models/product.js
+++ models/product.js
@@ -1,25 +1,24 @@
 var mongoose = require('mongoose');
 
 var Product = mongoose.model('Product', {
-	name: {
-		type: String,
-		required: true,
-		minlength: 1,
-		trim: true
-	},
-	price: {
-		type: Number,
-		required: true,
-	},
-	description: {
-        type: String,
-		trim: true
-	},
-	is_active: {
-		type: Boolean,
-		default: true
-	}
-
+  name: {
+    type: String,
+	required: true,
+    minlength: 1,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  is_active: {
+    type: Boolean,
+    default: true
+  }
 });
 
 module.exports = {Product};
