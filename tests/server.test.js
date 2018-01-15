4c3162d3176061eae07179374c7a185c7ed1a77c
Daya Sagar Nune <daya.webtech@gmail.com>
2018-01-14 23:44:05 -0800

    add some new tests and their dependency packages

---
+++ tests/server.test.js
@@ -0,0 +1,29 @@
+var {app} = require('../server');
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('APIs of Products Catalog', () => {
+
+    it.skip('FIX ME: should create new product', (done) => {
+
+      var product_details = {
+        name: "chai cup",
+        price: "4.00",
+        description: "adding from unit test"
+      };
+      chai
+        .request('app')
+  	    .post('/products')
+  	    .send(product_details)
+  	    .end((err, res) => {
+  	    	res.should.have.status(200);
+  	    	done();
+  	    });
+    });
+
+});
+
