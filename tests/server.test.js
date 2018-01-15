59fddf33436edec49a4a17f133288ee3f3666f66
Daya Sagar Nune <daya.webtech@gmail.com>
2018-01-14 23:44:58 -0800

    fix indentation

--- tests/server.test.js
+++ tests/server.test.js
@@ -7,9 +7,7 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('APIs of Products Catalog', () => {
-
     it.skip('FIX ME: should create new product', (done) => {
-
       var product_details = {
         name: "chai cup",
         price: "4.00",
