a0790d4007b5d24242085a7a01f8dd7ab9e9427b
Daya Sagar Nune <daya.webtech@gmail.com>
2018-01-14 16:24:23 -0800

    fix mongoose warning to use 'useMongoClient'

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ecomm_db')
+mongoose.connect('mongodb://localhost:27017/ecomm_db', {useMongoClient: true})
 
 module.exports = {mongoose};
