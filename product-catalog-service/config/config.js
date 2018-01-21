ad9d9b69133cd00824ed788c0090fef048bc29cd
Daya Sagar Nune <chantidaya@gmail.com>
2018-01-20 23:16:15 -0800

    delete files in old paths

--- product-catalog-service/config/config.js
+++
@@ -1,9 +0,0 @@
-var env = process.env.NODE_ENV || 'development'
-
-if ( env === 'development' ) {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://mongo:27017/QuickBuy';
-} else if ( env == 'production' ) {
-    //process.env.PORT = xxxx;
-    //process.env.MONGODB_URI = 'mongodb://mongodb:27017/xxxxxxx'
-}
